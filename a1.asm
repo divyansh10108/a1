d783515682b6c92200ee048406f1441aacc3e24f
SilmeyButter <95627241+divyansh10108@users.noreply.github.com>
2022-09-20 17:44:44 +0530

    Update a1.asm

--- a1.asm
+++ a1.asm
@@ -1,47 +1,21 @@
-extern scanf,printf
-section .data
-        line db "Please enter input: "
-        formatStr db "%s", 0
-        str times 64 db 0  
+extern printf
+extern scanf
 
-
-section .bss
-    name resb 32
-
+section .data 
+    line: db "Please enter your input "
+    s times 100 0
+    formatStr: db "%s", 0
 
 section .text
     global main
 
 main:
-    push rbp
-    xor rax, rax
-    call _printText1
-    call _getName
-    call _printName
-    pop rbp
-    mov rax, 0
-    ret
-    
-
-
-_getName:
-    mov rdi, formatStr
-    mov rsi, str
-    xor rax, rax
+    lea rdi, [formatStr]
+    lea rdx , [s]
+    xor eax, eax
     call scanf
-    ret
-
-
-_printText1:
-    mov rdi, 1
-    mov rsi, line
-    xor rax,rax
+    lea rdi, [formatStr]
+    lea rdx , [s]
+    xor eax, eax
     call printf
-    ret
 
-_printName:
-    mov rdi, formatStr
-    mov rsi, str
-    xor rax, rax
-    call printf
-    ret
