8daf126800776fab3f5118729d3334e145f5d6b9
SilmeyButter <95627241+divyansh10108@users.noreply.github.com>
2022-09-20 19:21:22 +0530

    Update a1.asm

--- a1.asm
+++ a1.asm
@@ -1,21 +1,55 @@
-extern printf
+global main
+extern printf  
 extern scanf
 
 section .data 
-    line: db "Please enter your input "
-    s times 100 0
-    formatStr: db "%s", 0
+    line: db "please enter yout input: "
+    s times 100 db 0
+    formatStr: db "%s" , 0
+    formatInt: db "%d" , 0
+    line2: db "please enter your integer: "
 
-section .text
-    global main
+section .text 
+    
+section .bss
+    loda resb 4
 
 main:
+
+    push rbp
+    mov rbp, rsp
+    sub rsp, 16
+
+    xor eax, eax 
+    lea rdi, [line]
+    call printf
+
     lea rdi, [formatStr]
-    lea rdx , [s]
-    xor eax, eax
-    call scanf
+    lea rsi, [s]
+    xor eax, eax     
+    call scanf   
+
     lea rdi, [formatStr]
-    lea rdx , [s]
+    lea rsi, [s]
     xor eax, eax
     call printf
+
+    xor eax, eax 
+    lea rdi, [line2]
+    call printf
+
+    lea rdi, [formatInt]
+    lea rsi, [loda]
+    xor eax, eax     
+    call scanf   
+
+    lea rdi, [formatInt]
+    lea rsi, [loda]
+    xor eax, eax
+    call printf
+
+    add rsp, 16
+    leave
+    ret
+
 
